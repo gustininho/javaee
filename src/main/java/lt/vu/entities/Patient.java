df758356c3ceec8e16381c06b2f91ec4f6fd54ba
gustininho <gustas.dersonas@gmail.com>
2022-05-25 03:13:27 +0300

    create entities

---
+++ src/main/java/lt/vu/entities/Patient.java
@@ -0,0 +1,44 @@
+package lt.vu.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name="PATIENT")
+public class Patient implements Serializable{
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="NAME", length=50, nullable=false, unique=false)
+    private String name;
+
+    public String sakykLabas() {
+        return "Labas " + new Date() + " " + toString();
+    }
+
+    public void init() {
+        System.out.println(toString() + " constructed.");
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
